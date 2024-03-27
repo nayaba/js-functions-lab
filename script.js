ae070285ac537d9768e918b99538d66d375d85bc
Nabila Ayaba <nabila.ayaba@gmail.com>
2024-03-27 13:57:58 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+Exercise 1: maxOfTwoNumbers
+
+In this exercise, create a function named maxOfTwoNumbers. It should take two numbers as inputs and return the larger number. If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x
+  } else {
+    return y
+  }
+
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(10, 3));
+
+/*
+Exercise 2: maxOfThree
+
+Define a function, maxOfThree, it should accept three numbers and return the largest among them.
+
+Complete Exercise 2 in the space below:
+*/
+
+
+
+console.log('Exercise 2 Result:', maxOfThree());
